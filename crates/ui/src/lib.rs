f6de0f09116157d11aff51ddd481242e40d7a7cc
Ari Weng <126212807+chybne@users.noreply.github.com>
2025-05-24 01:32:31 -0400

    changed the return type of view for testing

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,22 +1,36 @@
-use iced::widget::{Container, container};
+use iced::widget::{container, text, Column, column, Container};
+use iced::overlay::Element;
 
-#[derive(Debug, Clone)]
-pub enum Message {}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+}
 
 #[derive(Default)]
-pub struct Rainstorm {}
+pub struct Rainstorm {
+    counter: u32,
+}
 
 impl Rainstorm {
-    pub fn update(&mut self, _message: Message) {}
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => self.counter += 1,
+        }
+    }
 
-    pub fn view(&self) -> Container<Message> {
-        container("hellow world")
+    pub fn view<'a>(&self) -> Column<Message> {
+        let content  = container("hellow world")
             .padding(10)
             .center(800)
-            .style(container::rounded_box)
-            .into()
+            .style(container::rounded_box);
+
+
+        let result = column![].push(content);
+
+        result
     }
-}
+}   
 
 #[cfg(test)]
 mod tests {
