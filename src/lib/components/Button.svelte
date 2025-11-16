f89522492f9760f87c4a496f998064aaa6b6ec63
Ari Weng <126212807+chybne@users.noreply.github.com>
2025-11-16 17:28:17 -0500

    switched to svelte

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,23 @@
+<!-- Button Component -->
+<script>
+    import { createEventDispatcher } from "svelte";
+    export let icon = null;
+</script>
+
+<button class="btn">
+    <svelte:component this={icon} size="32" />
+</button>
+
+<style>
+    .btn:hover {
+        background-color: var(--background-dark);
+    }
+
+    .btn {
+        border: 0.2rem solid var(--text);
+        border-radius: 1rem;
+        margin: 0.2rem;
+        background-color: var(--background);
+        color: var(--text);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Brush } from "@lucide/svelte";
+    import Button from "$lib/components/Button.svelte";
+</script>
+
+<main class="container">
+    <div class="toolbar">
+        <div class="tools">
+            <Button icon={Brush} />
+        </div>
+    </div>
+</main>
+
+<style>
+    .toolbar {
+        display: flex;
+        align-items: center;
+        border: 0.2rem solid var(--text);
+        height: 3rem;
+        background-color: var(--background);
+    }
+</style>
