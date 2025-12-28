17f5156455d7e08bce47719793a9fe4f8ff48492
Ari Weng <126212807+chybne@users.noreply.github.com>
2025-12-28 16:44:11 -0500

    removed warnings

--- src-tauri/src/input.rs
+++ src-tauri/src/input.rs
@@ -1,6 +1,6 @@
 pub mod canvas_input;
 
-use canvas::{brush::stroke::StrokeManager, Canvas};
+use canvas::Canvas;
 use canvas_input::CanvasInput;
 use std::sync::{Arc, Mutex};
 use tauri::Window;
@@ -34,13 +34,4 @@ fn handle_zoom(zoom: f32, canvas: &mut Canvas) {
 
 fn handle_pan(offset_x: f32, offset_y: f32, canvas: &mut Canvas) {
     canvas.set_offset(offset_x, offset_y);
-}
-
-fn handle_begin_stroke(
-    mouse_x: f32,
-    mouse_y: f32,
-    pressure: f32,
-    stroke_manager: &mut StrokeManager,
-) {
-    stroke_manager.begin_stroke(mouse_x, mouse_y, pressure);
 }
